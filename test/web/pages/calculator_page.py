5363faebe3e0e6949b55032b0f50747b87e6f4a0
Oscar <asos.oscar@hotmail.com>
2025-09-21 23:10:20 +0200

    End-2-end testing

---
+++ test/web/pages/calculator_page.py
@@ -0,0 +1,53 @@
+from playwright.sync_api import Page
+from test.web.pages.page_base import PageBase
+
+class CalculatorPage(PageBase):
+    def __init__(self, page: Page) -> None:
+        super().__init__(page, 
+        elements={            
+            "username": "#user-name", 
+            "calculator_screen": "#calculator-screen",
+
+            "key_0": "#key-0",
+            "key_1": "#key-1",
+            "key_2": "#key-2",
+            "key_3": "#key-3",
+            "key_4": "#key-4",
+            "key_5": "#key-5",
+            "key_6": "#key-6",
+            "key_7": "#key-7",
+            "key_8": "#key-8",
+            "key_9": "#key-9",
+
+            "key_add": "#key-add",
+            "key_subtract": "#key-subtract",
+            "key_multiply": "#key-multiply",
+            "key_divide": "#key-divide",
+
+            "key_equals": "#key-equals",
+            "key_clear": "#key-clear",
+
+            "toggle_button": "#toggle-button",
+            "history": "#history",
+        })
+
+    def click_number(self, number):
+        self.element(f"key_{number}").click()
+
+    def click_operation(self, operation):
+        operation_map = {
+            "+": "key_add",
+            "-": "key_subtract", 
+            "*": "key_multiply",
+            "/": "key_divide"
+        }
+        self.element(operation_map[operation]).click()
+
+    def get_screen_value(self):
+        return self.element("calculator_screen").input_value()
+
+    def clear_screen(self):
+        self.element("key_clear").click()
+
+    def open_history(self):
+        self.element("toggle_button").click()
---
+++ test/web/test_web.py
@@ -0,0 +1,95 @@
+from test.web.test_base import WebBase
+from test.web.pages.login_page import LoginPage
+from test.web.pages.register_page import RegisterPage
+from test.web.pages.calculator_page import CalculatorPage
+from playwright.sync_api import expect
+import pytest
+
+class TestWeb(WebBase):
+    def test_login(self):
+        LoginPage(self.page).login(username="admin", password="test1234")
+        expect(CalculatorPage(self.page).element("username")).to_have_text("admin")
+
+    def test_register_new_user(self):
+        register_page = RegisterPage(self.page)
+        register_page.register(username="hello12345", password="hi21")
+        expect(CalculatorPage(self.page).element("username")).to_have_text("hello12345")
+
+    
+    def test_calculator_add(self):
+        calc_page = CalculatorPage(self.page)
+        calc_page.clear_screen()
+        
+        #5 + 3 = 8
+        calc_page.click_number(5)
+        calc_page.click_operation("+")
+        calc_page.click_number(3)
+        calc_page.element("key_equals").click()
+        
+        # verify
+        expect(calc_page.element("calculator_screen")).to_have_value("8")
+
+    def test_calculator_subtract(self):
+        calc_page = CalculatorPage(self.page)
+        calc_page.clear_screen()
+        
+        #9 - 4 = 5
+        calc_page.click_number(9)
+        calc_page.click_operation("-")
+        calc_page.click_number(4)
+        calc_page.element("key_equals").click()
+        
+        #verify
+        expect(calc_page.element("calculator_screen")).to_have_value("5")
+
+    def test_calculator_multiply(self):
+        calc_page = CalculatorPage(self.page)
+        calc_page.clear_screen()
+        
+        #6 * 7 = 42
+        calc_page.click_number(6)
+        calc_page.click_operation("*")
+        calc_page.click_number(7)
+        calc_page.element("key_equals").click()
+        
+        # asser
+        expect(calc_page.element("calculator_screen")).to_have_value("42")
+
+    def test_calculator_divide(self):
+        calc_page = CalculatorPage(self.page)
+        calc_page.clear_screen()
+        
+        # 8/2 =4
+        calc_page.click_number(8)
+        calc_page.click_operation("/")
+        calc_page.click_number(2)
+        calc_page.element("key_equals").click()
+        
+        # asser
+        expect(calc_page.element("calculator_screen")).to_have_value("4")
+
+
+    def test_history_feature(self):
+        #login
+        login_page = LoginPage(self.page)
+        login_page.login(username="admin", password="test1234")
+        
+        #initiate calc
+        calc_page = CalculatorPage(self.page)
+        
+        # calc 1
+        calc_page.click_number(3)
+        calc_page.click_operation("+")
+        calc_page.click_number(3)
+        calc_page.element("key_equals").click()
+        
+        #calc 2
+        calc_page.click_number(5)
+        calc_page.click_operation("-")
+        calc_page.click_number(2)
+        calc_page.element("key_equals").click()
+        
+        #history
+        calc_page.open_history()
+        #assert
+        expect(calc_page.element("history")).to_have_value("3+3=6\n5-2=3\n")
